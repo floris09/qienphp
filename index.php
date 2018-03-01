180deb4abffcefed093d76052e0478c3874b322b
floris09 <florismeininger@gmail.com>
2018-03-01 15:47:11 +0100

    rekenmachine

--- index.php
+++ index.php
@@ -1,17 +1,31 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+session_start();
+
+$number1 = $_SESSION['result'];
+$number = (int)$_GET['number'];
+$operator = $_GET['operator'];
+
+switch($operator){
+    case "+":
+        $result = $number1 + $number;
+        echo $result;
+        break;
+    case "-":
+        $result = $number1 - $number;
+        echo $result;
+        break;
+    case "*":
+        $result = $number1 * $number;
+        echo $result;
+        break;
+    case "/":
+        $result = $number1 / $number;
+        echo $result;
+        break;
+    case "%":
+        $result = $number1 % $number;
+        echo $result;
+}
+
+$_SESSION['result'] = $result;
+?>
