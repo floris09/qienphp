e1105f5f4a962d2a01b2e6862903f83331c5fb59
floris09 <florismeininger@gmail.com>
2018-03-08 14:29:38 +0100

    add bbqbaar interface

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
 
-$boerderij = new Kinderboerderij();
-$kip = new Kip();
-$kip->naam = "Kip Jan";
-$varken = new Varken();
-$varken->naam = "Varken Henk";
-
-$boerderij->voerRonde($kip);
-$boerderij->voerRonde($varken);
-
-
 class Kinderboerderij {
 	public function voerRonde(Dier $dier){
-		echo "We doen een voerronde voor: $dier->naam <br>";
+		echo "We doen een voerronde voor: $dier->naam --->";
+		$dier->eten();
+		echo "<br>";
 	}
+
+	public function barbeque(BBQBaar $dier){
+		$dier->bbqen();
+	}
+}
+
+interface BBQBaar{
+	public function bbqen();
 }
 
 abstract class Dier {
@@ -26,19 +26,47 @@ abstract class Dier {
 	}
 }
 
-class Varken extends Dier {
+class Varken extends Dier implements BBQBaar {
 	public $spekDikte;
 
 	public function knorren(){
 		echo "knor";
 	}
+
+	public function bbqen(){
+		echo "Varken op de BBQ";
+	}
 }
 
-class Kip extends Dier {
+class Kip extends Dier implements BBQBaar {
 	public $kleurVeren;
 
 	public function kakelen(){
 		echo "Kakaa";
 	}
 
+	public function bbqen(){
+		echo "Kip op de BBQ";
+	}
+
 }
+
+class Spin extends Dier {
+	public $poten;
+
+	public function springen(){
+		echo "Spring";
+	}
+}
+
+$boerderij = new Kinderboerderij();
+$kip = new Kip();
+$kip->naam = "Kip Jan";
+$varken = new Varken();
+$varken->naam = "Varken Henk";
+$spin = new Spin();
+$spin->naam = "Spin Klaas";
+
+$boerderij->voerRonde($kip);
+$boerderij->voerRonde($varken);
+$boerderij->barbeque($varken);
